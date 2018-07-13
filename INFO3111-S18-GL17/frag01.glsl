a363aa20f7f7f953a8d02e430020e2ed66c2a877
Michael Feeney <mfeeney@fanshawec.ca>
2018-07-13 13:48:03 -0400

    Added versions to the shaders

--- INFO3111-S18-GL17/frag01.glsl
+++ INFO3111-S18-GL17/frag01.glsl
@@ -1,4 +1,11 @@
+#version 420
+// If 420 doesn't work, try 330 
+// After 3.x, it is the version, so 4.20
+// Also, the version of GLSL WASN'T 
+// the same as the OpenGL version.
+
 varying vec3 color;
+
 void main()
 {
     gl_FragColor = vec4(color, 1.0);
--- INFO3111-S18-GL17/vert01.glsl
+++ INFO3111-S18-GL17/vert01.glsl
@@ -1,8 +1,14 @@
-uniform mat4 MVP;
-uniform vec3 meshColour; 
+#version 420
+// If 420 doesn't work, try 330 
+
 attribute vec3 vCol;		// float r, g, b;
 attribute vec3 vPos;		// float x, y, z;
+
+uniform mat4 MVP;
+uniform vec3 meshColour; 
+
 varying vec3 color;
+
 void main()
 {
     vec3 newVertex = vPos;				
