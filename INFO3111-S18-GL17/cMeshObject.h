a0e815ef1eb7f383d983ce95d249708fe830ddb7
Michael Feeney <mfeeney@fanshawec.ca>
2018-07-13 10:50:58 -0400

    Initial commit, after day 3

---
+++ INFO3111-S18-GL17/cMeshObject.h
@@ -0,0 +1,22 @@
+#ifndef _cMeshObject_HG_
+#define _cMeshObject_HG_
+
+#include <string>
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+
+class cMeshObject
+{
+public:
+	cMeshObject();
+    ~cMeshObject();
+    std::string meshName;
+    glm::vec3 pos;			// 0,0,0 (origin)
+    glm::vec4 colour;		// 0,0,0,1 (black)
+	glm::vec3 orientation;	// 
+    float scale;			// 1.0f
+    bool isWireframe;		// false
+};
+
+#endif // _cMeshObject_HG_
